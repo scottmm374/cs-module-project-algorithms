637becbbbd9635a61c2cdf6fd1e38bab16798887
Michelle Scott <scottmm374@gmail.com>
2020-09-10 17:46:38 -0400

    pass small test, but not large?

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,47 +4,20 @@ Returns: an integer
 '''
 
 
-# def climb_stairs(current, desired):
-#     print(current)
-#     if current > desired:
-#         return 0
-#     if current == desired:
-#         return 1
-
-#     return climb_stairs(current + 1, desired) + climb_stairs(current + 2, desired)
-
-def climb_stairs(current, desired, cache={}):
-    if current > desired:
+def eating_cookies(n):
+    if n < 0:
         return 0
-    if current == desired:
+    if n == 0:
         return 1
-    if current not in cache:
-        cache[current] = climb_stairs(
-            current + 1, desired, cache) + climb_stairs(current + 2, desired, cache)
-    return cache[current]
+
+    return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
 
 
-climb_stairs(0, 5)
+if __name__ == "__main__":
+    #     # Use the main function here to test out your implementation
+    num_cookies = 5
 
-
-# def eating_cookies(n):
-#     cookies = 0
-#     while cookies <= n:
-#         cookies = randint(1, 3)
-#     ways_to_eat = 0
-#     if cookie > n:
-#         return 0
-#     if cookie <= n:
-#         return eat_cookies
-#         ways_to_eat += 1
-
-
-#     # eat 1 cookies, eat 2 cookies, eat 3 cookies
-
-
-# if __name__ == "__main__":
-#     # Use the main function here to test out your implementation
-#     num_cookies = 5
-
-#     print(
-#         f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+# x = eating_cookies(5)
+# print(x)
